a78b9feb15a43c3f260dd0ed2dd5ffc64dee234a
mirzam-muhammad <mirzam.muhammad@gmail.com>
2025-08-21 21:33:15 +0800

    Memory upload

--- src/routes/uploadRouter.js
+++ src/routes/uploadRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const multer = require('multer');
+const storage = multer.memoryStorage();
 const uploadController = require('../controllers/uploadController');
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ storage });
 
 router.post('/', upload.single('gambar'), uploadController.uploadFile)
 
